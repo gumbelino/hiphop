8aacf8d784afc9ea1fa921283f094fdd9abc1d7d
Gustavo Umbelino <gumbelin@ucsd.edu>
2019-03-21 18:55:19 -0700

    Writting once instead of every loop
    Try that!

--- librosa_folk_mac.py
+++ librosa_folk_mac.py
@@ -27,14 +27,14 @@ header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff
 for i in range(1, 21):
     header += f' mfcc{i}'
 header += ' hiphopiness speechiness'
-header = header.split()
+header = header.split().join(',')
 
 
 # Writing data to csv file
-file = open('data_folk.csv', 'w', newline='')
-with file:
-    writer = csv.writer(file)
-    writer.writerow(header)
+file = open('data_folk.csv')
+file.write(header)
+to_write = ""
+
 for filename in os.listdir(f'/Users/norzvic/Music/folk'):
     if filename.endswith('.mp3'):
         songname = f'/Users/norzvic/Music/folk/{filename}'
@@ -49,13 +49,10 @@ for filename in os.listdir(f'/Users/norzvic/Music/folk'):
         to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
         for e in mfcc:
             to_append += f' {np.mean(e)}'
-        file = open('data_folk.csv', 'a', newline='')
-        with file:
-            writer = csv.writer(file)
-            writer.writerow(to_append.split())
-        continue
-    else:
-        continue
+        to_write += to_append.join(',') + "\n"
+        
+file.write(to_write)
+file.close()
 
 data = pd.read_csv('data_folk.csv')
 data.head()
